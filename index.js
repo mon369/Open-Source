4c964ff051e9948ea312cc3600b014706018daea
Simon <simoninoc@outlook.com>
2018-02-01 22:59:33 -0500

    File Upload Working, License Added

--- index.js
+++ index.js
@@ -5,46 +5,63 @@ const parser = require('./parse.js');
 var multer = require('multer');
 const HTTP_PORT = process.env.PORT || 8080;
 const path = require("path");
-
+const fs = require("fs");
 const PATTERN = /[0-9]/g;
-
 
 const storage = multer.diskStorage({
     destination: "./uploads",
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 })
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-app.get("/api/phonenumbers/parse/text/:parse", (req, res) =>{
+app.get("/api/phonenumbers/parse/text/:parse", (req, res) => {
     let phoneNumber = req.params.parse.match(PATTERN);
     phoneNumber = phoneNumber.join("");
     let phone = [];
-    parser.parse(phoneNumber).then((formattedNumber)=>{
+    parser.parse(phoneNumber).then((formattedNumber) => {
         phone.push(formattedNumber);
         res.send(phone);
-    }).catch((err) =>{
+    }).catch((err) => {
         phone.push(err)
         res.send(phone);
     })
 })
 
 
-
-app.get("/api/phonenumbers/parse/file", (req, res) =>{
+app.get("/api/phonenumbers/parse/file", (req, res) => {
     res.sendFile(__dirname + "/views/form.html");
 })
 
-app.post("/api/phonenumbers/parse/file", upload.single("parse"), (req, res) =>{
-   
-    res.send("You uploaded... " + req.file.filename);
+app.post("/api/phonenumbers/parse/file", upload.single("parse"), (req, res) => {
+    let filePathToRead = path.join(__dirname, "uploads", req.file.filename)
+    let fileContent = fs.readFileSync(filePathToRead, "UTF8").split('\n');
+    var filteredNumbers = [];
+    if (fileContent) {
+        for(line = 0; line < fileContent.length; line++){
+            let number = (fileContent[line].match(PATTERN));
+            if(number){
+                number = number.join("");
+                filteredNumbers.push(number);
+            }
+        }            
+        parser.parseFile(filteredNumbers).then((validPhones) => {
+            console.log(validPhones);
+            res.send(validPhones);
+        }).catch((err) => {
+            res.send(err);
+        })
+    }else{
+        res.status(404).send("Error: File is empty");
+    }
+    fs.unlinkSync(filePathToRead);
 })
 
-app.use("*",  (req, res)=>{
+app.use("*", (req, res) => {
     res.status(404).send("Can't Find What you're looking for");
 })
-app.listen(HTTP_PORT, ()=>{
+app.listen(HTTP_PORT, () => {
     console.log("Running " + HTTP_PORT)
 })
--- parse.js
+++ parse.js
@@ -2,26 +2,37 @@ const PNF = require('google-libphonenumber').PhoneNumberFormat;
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 const COUNTRY_CODE = "CA";
 
-module.exports.parse = function (phoneNumber){
-    return new Promise((resolve, reject)=>{
+module.exports.parse = function (phoneNumber) {
+    return new Promise((resolve, reject) => {
         const number = phoneUtil.parseAndKeepRawInput(phoneNumber, COUNTRY_CODE);
-        if(
-            phoneUtil.isPossibleNumber(number) && 
-            phoneUtil.isValidNumber(number) && 
-            phoneUtil.isValidNumberForRegion(number, COUNTRY_CODE)){
-            const formattedNumber  = phoneUtil.format(number, PNF.NATIONAL);
-            console.log(formattedNumber);
+        if (isValid(number)){
+            const formattedNumber = phoneUtil.format(number, PNF.NATIONAL);
             resolve(formattedNumber);
-        }else{
+        } else {
             reject("Phone is invalid")
         }
     })
 }
 
-module.exports.parseFile = function(data){
-
-    return new Promise((resolve, reject) =>{
-        
-
+module.exports.parseFile = function (data) {
+    let validPhones = [];
+    return new Promise((resolve, reject) => {   
+        for(i = 0; i < data.length; i++){
+            console.log(data[i]);
+            const number = phoneUtil.parseAndKeepRawInput(data[i], COUNTRY_CODE);
+            if (isValid(number)) {
+                validPhones.push(phoneUtil.format(number, PNF.NATIONAL));
+            }
+        }     
+        if(validPhones){
+            resolve(validPhones);
+        }else{
+            reject("No Valid Phones found");
+        }
     })
+}
+
+
+function isValid(number){
+    return phoneUtil.isPossibleNumber(number) && phoneUtil.isValidNumber(number) && phoneUtil.isValidNumberForRegion(number, COUNTRY_CODE)
 }
